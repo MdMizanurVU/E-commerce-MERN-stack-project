2b429ea8fdc14fb934495eb0d20d28ebb24ca31c
Md Mizanur Rahman <rmanmizanur@gmail.com>
2026-01-06 01:43:37 +1000

    Add CRUD API endpoints and update documentation
    - Add POST, PUT, and DELETE endpoints to /test route
    - All endpoints return appropriate HTTP status responses
    - Update server README.md with API testing and middleware learning resources
    - Prepare endpoints for Postman API testing
    - Complete basic HTTP methods implementation for future e-commerce routes

--- server/README.md
+++ server/README.md
@@ -5,3 +5,5 @@
 1. Express server setup
 2. HTTP Request and response
 3. nodemon and morgan package
+4. API testing with postman
+5. Middleware - https://www.youtube.com/watch?v=J8B530IdEjc
--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,24 @@ app.get("/test", (req, res) => {
   res.status(200).send({ message: "api testing is not working fine" });
 });
 
+app.post("/test", (req, res) => {
+  res
+    .status(200)
+    .send({ message: "post method api testing is not working fine" });
+});
+
+app.put("/test", (req, res) => {
+  res
+    .status(200)
+    .send({ message: "put method api testing is not working fine" });
+});
+
+app.delete("/test", (req, res) => {
+  res
+    .status(200)
+    .send({ message: "delete method api testing is not working fine" });
+});
+
 app.listen(3001, () => {
   console.log("Server is running at http://localhost:3001");
 });
